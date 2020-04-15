9831f34b286f23c02264b419f8183e0530ba257d
Mario Vercellotti <mario@robocorp.com>
2020-04-15 12:46:55 +0300

    Add explanation for variables, change city names, change template

--- variables/variables.py
+++ variables/variables.py
@@ -2,10 +2,8 @@ from robot.libraries.OperatingSystem import OperatingSystem
 
 os = OperatingSystem()
 
-# EXCEL_FILE_PATH = "./devdata/Data.xlsx"
-# PDF_TEMPLATE_PATH = "./devdata/invite.template"
-# PDF_OUTPUT_DIRECTORY = "./output/"
-
+# To make sure that the folders stay the same no matter the environment where we execute our robot, we
+# will use the normalize_path function provided by the Operating System robotframework library.
 EXCEL_FILE_PATH = os.normalize_path(__file__ + "/../../devdata/Data.xlsx")
 PDF_TEMPLATE_PATH = os.normalize_path(
     __file__ + "/../../devdata/invite.template")
